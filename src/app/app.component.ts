49ab3c9e011d69926c163fa0d1446d684e4048e1
Henning Ejnefjäll <henning.ejnefjall@gmail.com>
2018-02-13 16:34:27 +0100

    another commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostId extends Post {
+  id: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: any; //Observable<Post[]>;
+  
+  title:string;
+  content:string;
+
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(private afs:AngularFirestore){
+
+  }
+
+  ngOnInit(){
+     this.postsCol = this.afs.collection('myposts');
+    //this.postsCol = this.afs.collection('myposts', ref => ref.where('title',"==", "omfg")); //where alternativ, orderby, limit etc.
+    // this.posts = this.postsCol.valueChanges();
+    this.posts = this.postsCol.snapshotChanges() //innehåller metadata ex. id
+      .map(actions => {
+        return actions.map(a=>{
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return {id, data};
+        })
+      })
+  }
+
+  addPost(){
+    // this.afs.collection('myposts').add({'title' : this.title, 'content':this.content});
+    this.afs.collection('myposts').doc('my-custom-id').set({'title' : this.title, 'content':this.content});
+  }
+
+  getPost(postId){
+    this.postDoc = this.afs.doc('myposts/'+postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId){
+    this.afs.doc('myposts/'+postId).delete();
+  }
 }
